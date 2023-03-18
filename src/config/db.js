032ea01bd562c4db556d94715cffbcac59efb8c5
umayanga1997 <umayanga.avi@gmail.com>
2023-03-18 18:53:20 +0530

    err object to error

--- src/config/db.js
+++ src/config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URL);
     console.log(`Mongodb Connected`);
   } catch (error) {
-    console.log(err);
+    console.log(error);
   }
 };
 
